3d070ed9362560d617fa12498dbd32c220fa8104
gigihsigap <gigihsigap@gmail.com>
2024-05-04 12:55:36 +0700

    Fixed DB migrations

--- db/migrations/000001_initialize_db.down.sql
+++ db/migrations/000001_initialize_db.down.sql
@@ -3,12 +3,12 @@ DROP INDEX IF EXISTS idx_cat_matches_all_columns;
 DROP INDEX IF EXISTS idx_cats_all_columns;
 
 -- Drop foreign key constraints
-ALTER TABLE cat_matches DROP CONSTRAINT IF EXISTS cat_matches_issuer_id_fkey;
-ALTER TABLE cat_matches DROP CONSTRAINT IF EXISTS cat_matches_receiver_id_fkey;
+ALTER TABLE matches DROP CONSTRAINT IF EXISTS matches_issuer_id_fkey;
+ALTER TABLE matches DROP CONSTRAINT IF EXISTS matches_receiver_id_fkey;
 ALTER TABLE cats DROP CONSTRAINT IF EXISTS cats_user_id_fkey;
 
 -- Drop tables
-DROP TABLE IF EXISTS cat_matches;
+DROP TABLE IF EXISTS matches;
 DROP TABLE IF EXISTS cats;
 DROP TABLE IF EXISTS users;
 
--- db/migrations/000001_initialize_db.up.sql
+++ db/migrations/000001_initialize_db.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id bigserial PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
@@ -11,40 +11,34 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TYPE race_enum AS ENUM ('Persian', 'Maine Coon', 'Siamese', 'Ragdoll', 'Bengal', 'Sphynx', 'British Shorthair', 'Abyssinian', 'Scottish Fold', 'Birman');
 CREATE TYPE sex_enum AS ENUM ('male', 'female');
 
-CREATE TABLE cats (
+CREATE TABLE IF NOT EXISTS cats (
     id SERIAL PRIMARY KEY,
-    user_id SERIAL NOT NULL,
     name VARCHAR(30) NOT NULL,
     race race_enum NOT NULL,
     sex sex_enum NOT NULL,
-    age_in_month INT NOT NULL,
+    age_in_months INT,
+    description VARCHAR(200),
     image_urls TEXT[],
-    description VARCHAR(200) NOT NULL,
-    has_matched BOOLEAN NOT NULL DEFAULT false,
+    user_id INT NOT NULL,
+    has_matched BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     deleted_at timestamp DEFAULT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE INDEX idx_cats_all_columns ON cats (name);
-
-CREATE TYPE status_match_enum AS ENUM ('pending', 'approved', 'rejected');
-
-CREATE TABLE cat_matches (
+CREATE TABLE IF NOT EXISTS matches (
     id SERIAL PRIMARY KEY,
-    issuer_id SERIAL NOT NULL,
-    receiver_id SERIAL NOT NULL,
-    match_cat_id SERIAL NOT NULL,
-    user_cat_id SERIAL NOT NULL,
-    message VARCHAR(120) NOT NULL,
-    status status_match_enum NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    match_cat_id INT NOT NULL,
+    user_cat_id INT NOT NULL,
+    is_approved BOOLEAN DEFAULT NULL,
+    message TEXT NOT NULL,
+    issued_by INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     deleted_at timestamp DEFAULT NULL,
-    FOREIGN KEY (issuer_id) REFERENCES users(id),
-    FOREIGN KEY (receiver_id) REFERENCES users(id)
+    FOREIGN KEY (issued_by) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (match_cat_id) REFERENCES cats(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_cat_id) REFERENCES cats(id) ON DELETE CASCADE
 );
-
-CREATE INDEX idx_cat_matches_all_columns ON cat_matches (user_cat_id, match_cat_id,status);
